66aca8d917ebb8da6af12414bf67e20b2ceed5f7
JamesHakim13 <aminuddinhakim@gmail.com>
2025-01-04 15:45:05 +0800

    small update in code

--- app/src/main/java/com/example/fauxly/ui/fragment/WordbankFragment.java
+++ app/src/main/java/com/example/fauxly/ui/fragment/WordbankFragment.java
@@ -1,4 +1,20 @@
 package com.example.fauxly.ui.fragment;
 
-public class WordbankFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fauxly.R;
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class WordbankFragment extends Fragment{
+    private static final String ARG_TITLE = "title";
+
 }
